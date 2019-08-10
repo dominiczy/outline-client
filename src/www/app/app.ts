c078aa2492a8c40a00ba23b00032f01bc023e45c
Dominic Zijlstra <d.zijlstra@iwoca.co.uk>
2019-08-10 13:11:09 +0100

    Undo domain renaming. Change version to 1.3

--- src/www/app/app.ts
+++ src/www/app/app.ts
@@ -405,7 +405,7 @@ export class App {
     console.warn('Payment event received');
     const amount = event.detail.amount;
     const currency = 'gbp';
-    const returnUrl = 'https://openinternetvpn.com/charge/';
+    const returnUrl = 'https://openinternetchina.com/charge/';
     const name = event.detail.name;
     const email = event.detail.email;
     cordova.plugins.stripe.setPublishableKey('pk_live_NkeUgHrAQQTovEgNI22z26qR00fbAacxAp');
